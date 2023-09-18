dde3947c845c6f5ec84c9d14a997da474507fdf9
Timmy Chen <timmy80713@gmail.com>
2023-09-18 22:17:02 +0800

    chore: adjust cloud function name for pubsub-ci project.

--- pubsub-ci/deploy.sh
+++ pubsub-ci/deploy.sh
@@ -2,7 +2,7 @@
 ./gradlew build
 
 REGION=asia-east1
-gcloud functions deploy slack-tools-ci \
+gcloud functions deploy pubsub-ci \
     --gen2 \
     --memory 256MB \
     --region ${REGION} \
@@ -14,7 +14,7 @@ gcloud functions deploy slack-tools-ci \
     --set-secrets "BITRISE_BUILD_TRIGGER_TOKEN=BITRISE_BUILD_TRIGGER_TOKEN:latest"
 
 REPOSITORY=gcf-artifacts
-PACKAGE=slack--tools--ci
+PACKAGE=pubsub--ci
 for VERSION in $(gcloud artifacts versions list --repository=${REPOSITORY} --location=${REGION} --package=${PACKAGE} --format='value(name)'); do
     gcloud artifacts versions delete ${VERSION} --quiet --repository=${REPOSITORY} --location=${REGION} --package=${PACKAGE}
 done
