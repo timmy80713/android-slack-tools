e2efc7f28a3ec554a657c659dc24cefa7162fd9e
Timmy Chen <timmy80713@gmail.com>
2025-01-31 11:16:46 +0800

    feat: adjust parameters for deleting artifacts in the slack-command-main project

--- slack-command-main/deploy.sh
+++ slack-command-main/deploy.sh
@@ -14,7 +14,7 @@ gcloud functions deploy slack-command-main \
     --set-secrets "SLACK_SIGNING_SECRET=SLACK_SIGNING_SECRET:latest"
 
 REPOSITORY=gcf-artifacts
-PACKAGE=slack--command--main
+PACKAGE=android--slack--tools__asia--east1__slack--command--main
 for VERSION in $(gcloud artifacts versions list --repository=${REPOSITORY} --location=${REGION} --package=${PACKAGE} --format='value(name)'); do
     gcloud artifacts versions delete ${VERSION} --quiet --repository=${REPOSITORY} --location=${REGION} --package=${PACKAGE}
 done
